4e0d68695967e218a4cfe3ff257c094c636528b6
Mujahed Ali <mujahed97@gmail.com>
2023-11-07 16:19:51 -0500

    Modules for Slidable and Stepable

---
+++ practice-for-ch-ruby-chess-long-practice-main/modules.rb
@@ -0,0 +1,17 @@
+module Slidable 
+
+    private
+    HORIZONTAL_DIRS = [
+        [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+        [0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7]
+    ]
+    DIAG_DIRS = [
+         
+
+    ]
+end 
+
+module Steppable
+
+
+end 
