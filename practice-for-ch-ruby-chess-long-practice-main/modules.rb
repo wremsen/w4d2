8b13358e928f1bf98d9ef0fe2ac12cef4ecbe753
wremsen <wcremsen@gmail.com>
2023-11-08 11:54:01 -0500

    close to done with slideable

--- practice-for-ch-ruby-chess-long-practice-main/modules.rb
+++ practice-for-ch-ruby-chess-long-practice-main/modules.rb
@@ -1,12 +1,35 @@
 module Slidable
 
+    HORIZONTAL_VERT_DIRS = [
+        [0, 1], [0, -1]
+        [1, 0]. [-1, 0]
+    ].freeze
+
+    DIAG_DIRS = [
+         [1, 1], [1, -1]
+         [-1, -1], [-1, 1]
+    ].freeze
+
+
+    def horizontal_vert_dirs
+        HORIZONTAL_VERT_DIRS
+    end
+
+    def diag_dirs
+        DIAG_DIRS
+    end
 
     def moves 
         moves = []
 
-        HORIZONTAL_VERT_DIRS.each do |ele|
+        self.move_dirs.each do |ele|
+            dx = ele[0]
+            dy = ele[1]
+            my_dir_moves = grow_unblocked_moves_in_dir(dx, dy)
+            moves + my_dir_moves
         end
 
+        moves
     end
 
 
@@ -14,14 +37,49 @@ module Slidable
     
 
     private
-    HORIZONTAL_VERT_DIRS = [
-        [0, 1], [0, -1]
-        [1, 0]. [-1, 0]
-    ]
-    DIAG_DIRS = [
-         [1, 1], [1, -1]
-         [-1, -1], [-1, 1]
-    ]
+    def move_dirs
+        raise NotImplementedError
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+        dir_moves = []
+
+        queue = [self.pos]
+        until queue.empty?
+            current_pos = queue.shift
+            delta = [dx, dy]
+            new_pos = current_pos.zip(delta).map  {|a, b| a + b}
+
+            if self.valid_move?(new_pos) == false
+                next
+            end
+
+            if !self.board[new_pos].is_a?(NullPiece) && self.board[new_pos].color == self.color
+                next
+            end
+
+            if !self.board[new_pos].is_a?(NullPiece) && self.board[new_pos].color != self.color
+                dir_moves << new_pos
+                break
+            end
+
+            dir_moves << new_pos
+            queue << new_pos
+        end
+
+        dir_moves
+    end
+
+
+
+
+
+
+
+        dir_moves
+    end
+
+
 end 
 
 module Steppable
--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,32 +1,35 @@
 class Piece
-    
-    
+
+    attr_reader :color, :pos, :board
+
     def initialize (color, board, pos)
         @color = color
         @board = board
         @pos = pos
+        @symbol = nil
     end 
 
     def to_s
-
-
+        @symbol.to_s
     end
 
     def empty?
 
     end 
 
-    def valid_moves
-
+    def valid_move?(pos)
+        row = pos[0]
+        col = pos[1]
+        return (0..7).to_a.include?(row) && (0..7).to_a.include?(col)
     end 
 
     def pos=(val)
         @pos = val
     end 
 
-    def symbol 
-        puts "symbol"
-    end 
+    # def symbol 
+    #     puts "symbol"
+    # end 
 
     private
     def move_into_check?(end_pos)
